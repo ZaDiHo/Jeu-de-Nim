44f682d88be47c33bab2c9fb73ac546d8bb9f8ef
ZaDiHo <contact@thomasprovost.fr>
2023-12-21 15:41:14 +0100

    Update LocalScreen.py

--- ui/LocalScreen.py
+++ ui/LocalScreen.py
@@ -10,9 +10,12 @@ def showLocalScreen():
     background.resize(1280,720)
 
     title = Text(823,85,"LOCAL",70,)
+    vsText = Text(250, 295, "CONTRE", 40)
+    firstPlayerText = Text(190, 250, "1. PLAYER_12345", 30)
+    secondPlayerText = Text(190, 370, "2. PLAYER_67891", 30)
     menuArea = Area(650,75,550,550)
 
-    nameArea = Area(350, 75, 550, 300)
+    nameArea = Area(100, 205, 500, 250)
 
     def settingsButtonEvent():
         showLocalSettingsScreen()
@@ -37,6 +40,9 @@ def showLocalScreen():
     screen.add_element(backButton)
     screen.add_element(title)
     screen.add_element(nameArea)
+    screen.add_element(vsText)
+    screen.add_element(firstPlayerText)
+    screen.add_element(secondPlayerText)
     
     screen.run()
 
